a3e5bc6bc119704397180ff0d44a60fc9598af26
Night-G <61281536+Night-G@users.noreply.github.com>
2020-04-19 21:18:35 +0300

    1

--- site/js/main.js
+++ site/js/main.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
 
-
     $('h1').mouseenter(function(){
         $('h1').css('color', '#660066');
     });
     $('h1').mouseleave(function(){
         $('h1').css('color','black');
     });
-    // hide кнопку после при ее нажатии  
+    // hide кнопку  при ее нажатии  
     $('Button').click(function(){
         $('Button').hide(100);
     });
